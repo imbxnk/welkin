9fbd888f6a373da80199c63c5793588dffd149e6
BankNeko <bankneko@gmail.com>
2021-01-29 02:12:01 +0700

    update api
    show not found if route does not exist

--- api/app.js
+++ api/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-const connectDatabase = require('./config/database');
-
 
 // Setting up config variables
+const dotenv = require('dotenv');
 dotenv.config({path: './config/.env'});
 
 // Connect to MongoDB
+const connectDatabase = require('./config/database');
 connectDatabase();
 
 // Importing all routes
@@ -15,6 +14,12 @@ const students = require('./routes/students');
 
 app.use('/v1', students);
 
+// Show "Not Found" if route does not exist
+app.get('*', function(req, res){
+    res.status(404).send('<pre style="word-wrap: break-word; white-space: pre-wrap;">Not Found</pre>');
+});
+
+// Start listening to PORT
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
