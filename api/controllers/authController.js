636cf2ae3fd1f6db79e8d181c8536f582f1edf04
BankNeko <bankneko@gmail.com>
2021-01-31 16:34:39 +0700

    update username genrator function

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,8 +3,25 @@ const catchAsyncErrors = require('../middlewares/catchAsyncError');
 
 // Create a new user => /v1/user/create
 exports.createUser = catchAsyncErrors( async (req, res, next) => {
-    const {usermame, password, first_name, middle_name, last_name, email, group, avatar_url} = req.body;
+    const {password, first_name, middle_name, last_name, email, group, isAdvisor} = req.body;
 
+    // Automatically create Username according to firstname.las
+    let firstname = first_name.trim() + '.';    
+    let lastname = last_name.trim();
+    let username = firstname + lastname.substring(0,3);
+    // If Lastname has more than or equal to 3 characters
+    if(lastname.length >= 3) {
+        // Check if the username exists
+        let user = await User.findOne({username: username});
+        // If exist use => firstname.lat instead
+        while(user && lastname.length > 3) {
+            lastname = lastname.slice(0,2) + lastname.slice(3);
+            username = firstname + '.' + lastname.substring(0,3);
+            user = await User.findOne({username: username});
+        }
+    }
+
+    // Create User
     const user = await User.create({
         username,
         password,
@@ -13,7 +30,7 @@ exports.createUser = catchAsyncErrors( async (req, res, next) => {
         last_name,
         email,
         group,
-        avatar_url
+        isAdvisor
     });
 
     res.status(200).json({
