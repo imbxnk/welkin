da0059489e88143a8e0e889c310ba7f5c4e662a4
phatac131 <phattharaporn.rod@student.mahidol.edu>
2021-03-08 03:03:19 +0700

    Update login.vue
    add axios post

--- main/src/components/login.vue
+++ main/src/components/login.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container">
-    <form @submit.prevent='handleSubmit'>
+  <div>
+    <form @submit.prevent="handleSubmit()">
         <h4>Login</h4>
         <div class="form-group">
             <label>Username</label>
-            <input type="username" class="form-control" v-model='username' placeholder="Username"/> 
+            <input class="form-control" v-model='username' placeholder="Username"/> 
         </div>
         <div class="form-group">
             <label>Password</label>
-            <input type='password' class="form-control" v-model='password' placeholder="Password"/>
+            <input type="password" class="form-control" v-model='password' placeholder="Password"/>
         </div>
         <button class="btn btn-primary btn-block">Login</button>
     </form>
+    <div>
+        <ul></ul>
+    </div>
   </div>
 </template>
 
@@ -20,26 +23,35 @@ import axios from 'axios'
 export default {
     data(){
         return{
-            username: '',
-            password: '',
+                username: "",
+                password: "",
+                token: "",
+                userId: "",
+                message: "",
         }
     },
     methods: {
         async handleSubmit(){
-            const response = await axios.post('https://api.welkin.app/v2/graphql', {
-                query : `
-                    mutation {
-                        login ( username : "phattharaporn.roe", password : "phatphat") {
-                            token
-                            userId
-                            message
-                        }
-                    }
-                `
-                }, 
-                { withCredentials : true }
-            )
-            console.log(response)
+            //axios post to check token, userId compare with the username and password
+            await axios.post("https://api.welkin.app/v2/graphql",{
+                        query: `
+                            mutation {
+                                login ( username : "${this.username}" , password : "${this.password}") {
+                                    token
+                                    userId
+                                    message
+                                }
+                                }
+                        `
+                    }  
+                )
+                // If axios success. let this.token this.userId this.message 
+                    .then(res => {
+                        console.log(res)
+                    })
+                    .catch(err => {
+                        console.log(err.message)
+                    })
         }
     },
 }
