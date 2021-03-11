7af6de1275efa4a71ff0d58c83496aa0e6f916fe
BankNeko <bankneko@gmail.com>
2021-03-11 12:59:59 +0700

    Update login.vue

--- main-v2/src/pages/login/login/login.vue
+++ main-v2/src/pages/login/login/login.vue
@@ -44,7 +44,7 @@ export default {
             .post(process.env.VUE_APP_GRAPHQL_URL,{ query }, { withCredentials: true })
             .then(res => {
               console.log(res)
-              window.location.replace = "/";
+              window.location.replace("/")
             })
             .catch(err => { console.log(err.message)})
       },
