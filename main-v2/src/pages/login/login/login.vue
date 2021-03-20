0ffe12c26786c5a0d151a013a2a3e7371d024b8c
nhanniee.s <nhanniee.s@gmail.com>
2021-03-20 14:32:10 +0700

    Update login.vue

--- main-v2/src/pages/login/login/login.vue
+++ main-v2/src/pages/login/login/login.vue
@@ -1,19 +1,34 @@
 <template>
   <v-main>
-    <v-card style="width: 600px; margin: 20px auto">
+    <v-row style="height: 100%" justify="center" align="center">
+      <v-col>
+    <v-card style="width: 400px; margin: 20px auto">
       <form @submit.prevent="handleSubmit()" style="padding: 30px">
-          <h4>Login</h4>
-          <div class="form-group">
-              <label>Username</label>
-              <input class="form-control" v-model='username' placeholder="Username"/>
-          </div>
-          <div class="form-group">
-              <label>Password</label>
-              <input type="password" class="form-control" v-model='password' placeholder="Password"/>
-          </div>
+          <h4 ou style="text-align:center">Login to your WELKIN account</h4>
+            <v-text-field
+              v-model="username"
+              label="Username"
+              outlined
+              class="pt-5"
+            ></v-text-field>
+
+                      <v-text-field
+              v-model="password"
+              class=""
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              label="Password"
+              hint="At least 4 characters"
+              outlined
+              @click:append="show1 = !show1"
+            ></v-text-field>
+
           <button class="btn btn-primary btn-block">Login</button>
       </form>
-    </v-card>
+    </v-card></v-col>
+    </v-row>
   </v-main>
 </template>
 
@@ -21,11 +36,17 @@
 export default {
   data(){
     return{
+            show1: false,
       username: "",
       password: "",
       token: "",
       userId: "",
       message: "",
+      rules:{
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 4 || "Min 4 characters",
+        emailMatch: () => "The email and password you entered don't match",
+      }
     }
   },
   methods: {
