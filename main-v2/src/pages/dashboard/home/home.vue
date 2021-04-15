06f342d79aaf750c666ff5fce94790dc64b4e4f1
BankNeko <bankneko@gmail.com>
2021-04-15 19:03:56 +0700

    update responsive home

--- main-v2/src/pages/dashboard/home/home.vue
+++ main-v2/src/pages/dashboard/home/home.vue
@@ -1,9 +1,9 @@
 <template>
         <v-row>
-          <v-col>
+          <v-col cols="12" lg="8" xl="9">
             <ShowStudentsTable></ShowStudentsTable>
           </v-col>
-          <v-col cols="12" sm="6" md="3" lg="3">
+          <v-col cols="12" lg="4" xl="3">
             <ShowBatchesSummary></ShowBatchesSummary>
           </v-col>
         </v-row>
