e433ce1f4147d4785af222a42a1e2f979cc71f13
BankNeko <bankneko@gmail.com>
2021-04-28 21:35:25 +0700

    Update login.vue

--- main-v2/src/pages/login/login.vue
+++ main-v2/src/pages/login/login.vue
@@ -2,28 +2,37 @@
   <v-main>
     <v-row style="height: 100%" justify="center" align="center">
       <v-col>
-        <v-card style="width: 400px; margin: 20px auto">
-          <form @submit.prevent="handleSubmit()" style="padding: 30px">
-            <h4 ou style="text-align:center">Login to your WELKIN account</h4>
-            <v-text-field v-model="username" label="Username" outlined class="pt-5"></v-text-field>
+        <v-card class="mx-auto my-4" style="width: 400px;" elevation="2">
+          <v-card-title>Log in</v-card-title>
+          <v-card-subtitle>Welcome to Welkin</v-card-subtitle>
+          <v-card-text>
+            <form @submit.prevent="login()">
+              <v-text-field
+                v-model="username"
+                label="Username"
+                outlined
+                class="pt-5"
+              ></v-text-field>
 
-            <v-text-field
-              v-model="password"
-              class=""
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="Password"
-              hint="At least 4 characters"
-              outlined
-              @click:append="show1 = !show1"
-            ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required]"
+                :type="show1 ? 'text' : 'password'"
+                name="input-10-1"
+                label="Password"
+                placeholder=" "
+                outlined
+                @click:append="show1 = !show1"
+              ></v-text-field>
+              <button class="btn btn-primary btn-block wk-login-btn">Login</button>
+            </form>
+          </v-card-text>
+          <v-card-actions>
 
-            <button class="btn btn-primary btn-block">Login</button>
-          </form>
-        </v-card></v-col
-      >
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-main>
 </template>
@@ -39,14 +48,12 @@ export default {
       userId: "",
       message: "",
       rules: {
-        required: (value) => !!value || "Required.",
-        min: (v) => v.length >= 4 || "Min 4 characters",
-        emailMatch: () => "The email and password you entered don't match",
+        required: (value) => !!value || "Required."
       },
     };
   },
   methods: {
-    async handleSubmit() {
+    login() {
       //axios post to check token, userId compare with the username and password
       let query = `
           mutation {
@@ -57,7 +64,7 @@ export default {
             }
           }
       `;
-      await this.axios
+      this.axios
         .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
         .then((res) => {
           console.log(res);
@@ -65,11 +72,19 @@ export default {
         })
         .catch((err) => {
           alert("Incorrect username or password, please try again");
-          console.log(err.message);
         });
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.wk-login-btn {
+  height: 48px;
+}
+.wk-login-btn:hover, .wk-login-btn:focus, .wk-login-btn:active {
+  outline:0px !important;
+  -webkit-appearance:none;
+  box-shadow: none !important;
+}
+</style>
