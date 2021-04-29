357284dcd716996b15f6fba7b4dfb8d043041a05
nhanniee.s <nhanniee.s@gmail.com>
2021-04-29 22:36:03 +0700

    import apexchart

--- main-v2/src/pages/main.js
+++ main-v2/src/pages/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "../plugins/vuetify";
-import router from "../router"
+import router from "../router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
@@ -9,11 +9,16 @@ import VueAxios from "vue-axios";
 // Import Bootstrap an BootstrapVue CSS files
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+//Import Apexchart
+import VueApexCharts from "vue-apexcharts";
 
 // Use BootstrapVue
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
+//Use Apexchart
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 // Use Axios
 Vue.use(VueAxios, axios);
 
@@ -22,5 +27,5 @@ Vue.config.productionTip = false;
 new Vue({
   vuetify,
   render: (h) => h(App),
-  router
+  router,
 }).$mount("#app");
