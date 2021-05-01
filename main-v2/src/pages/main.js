83b37294fcfe31ca82f9eb7b06daa34a1b099275
Miqly <42849871+knmixsiri@users.noreply.github.com>
2021-05-01 21:42:20 +0700

    Update main.js

--- main-v2/src/pages/main.js
+++ main-v2/src/pages/main.js
@@ -6,6 +6,21 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+// Import firebase
+import firebase from 'firebase'
+var firebaseConfig = {
+  apiKey: "AIzaSyAu5EqqCHUxNeS_-oXsp3ISgdBk-5z8YDo",
+  authDomain: "welkin-d9b90.firebaseapp.com",
+  projectId: "welkin-d9b90",
+  storageBucket: "welkin-d9b90.appspot.com",
+  messagingSenderId: "305194504139",
+  appId: "1:305194504139:web:dcfc7a5ab70b8bc3a9f04e",
+  measurementId: "G-4C2RBJ6T8N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 // Import Bootstrap an BootstrapVue CSS files
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
