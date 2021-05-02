308a8350116e040425f1bbe11b74084b2700e691
nhanniee.s <nhanniee.s@gmail.com>
2021-05-02 21:36:24 +0700

    stdinfo in course detail

--- main-v2/src/pages/dashboard/courselist/components/student_info.vue
+++ main-v2/src/pages/dashboard/courselist/components/student_info.vue
@@ -3,16 +3,14 @@
   <div>
     <v-card-title>
       <!-- {{ class_detail.year }}T{{ class_detail.trimester }} Section{{ class_detail.section }} -->
-      {{ stdList.year }}{{ stdList.trimester }}<br />
-      {{ stdList._id }}<br />
-      {{ text }}
+      {{ class_detail.year }}T{{ class_detail.trimester }} SEC{{ class_detail.section }}
     </v-card-title>
     <v-card>
       <v-data-table v-if="loading" loading loading-text="Loading... Please wait"></v-data-table>
       <v-data-table
         v-else
         :headers="headers"
-        :items="students"
+        :items="class_detail.enrollments"
         mobile-breakpoint="680"
         class="home"
       ></v-data-table>
@@ -24,8 +22,10 @@
 export default {
   name: "students_table",
   components: {},
-  props: ["classID", "stdList"],
-
+  props: ["classID"],
+  created() {
+    this.loadStudent(this.$props.classID);
+  },
   data() {
     return {
       dialog: false,
@@ -47,23 +47,52 @@ export default {
   },
   watch: {
     classID: function(current) {
-      this.loadClass(current);
+      this.loadStudent(current);
     },
   },
   methods: {
-    loadClass(i) {
-      if (i.toString() == this.stdList._id) {
-        this.text = "class code matches!";
-        console.log(this.stdList);
-        this.students = this.stdList.enrollments;
-        console.log(this.stdList.year + "T" + this.stdList.trimester);
-        console.log("1st student must be : " + this.stdList.enrollments[0].student.name);
-        this.loading = false;
-      } else {
-        this.text = "class code is not match!";
-        console.log("current id : " + i.toString());
-        console.log("this data is for id:" + this.stdList._id);
-      }
+    loadStudent(class_id) {
+      let query = `
+                    query {
+                      class(classId: "${class_id}") {
+                         _id
+                        course {
+                          code
+                          name
+                          description
+                        }
+                        instructor {
+                          title
+                          name
+                        }
+                        section
+                        trimester
+                        year
+                        enrollments {
+                          student {
+                            sid
+                            given_name
+                            family_name
+                            email
+                          }
+                          grade
+                        }
+                      }
+                    }
+                `;
+      this.axios
+        .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
+        .then((res) => {
+          this.class_detail = res.data.data.class;
+          this.class_detail.enrollments.forEach((enrollment) => {
+            enrollment.student.name =
+              enrollment.student.given_name + " " + enrollment.student.family_name;
+          });
+          console.log(this.class_detail._id);
+          console.log(this.class_detail.enrollments[0].student.name);
+          this.loading = false;
+        })
+        .catch((err) => {});
     },
   },
 };
