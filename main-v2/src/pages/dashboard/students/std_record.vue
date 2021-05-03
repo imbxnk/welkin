a3fcc4f2eae7a2513d35fdd31737f1ea36f34b12
nhanniee.s <nhanniee.s@gmail.com>
2021-05-03 21:32:14 +0700

    Update std_record.vue

--- main-v2/src/pages/dashboard/students/std_record.vue
+++ main-v2/src/pages/dashboard/students/std_record.vue
@@ -1,8 +1,53 @@
 <template>
   <div>
-    <v-btn @click="$router.push('/students/all')">back to course list</v-btn>
-    hello this is std rec {{ this.$route.params.sid }}<br />
-    {{ this.students.given_name }} {{ this.students.family_name }}
+    <v-btn @click="$router.push('/students/all')" class="ma-3">back to student list</v-btn>
+    <!-- hello this is std rec {{ this.$route.params.sid }}<br />
+    {{ this.students.given_name }} {{ this.students.family_name }}<br />
+    {{ this.students.entry_year }}T{{ this.students.entry_trimester }}<br />
+    Email: {{ this.students.email }}<br />
+    Line id: {{ this.students.lineID }}<br />
+    status: {{ this.students.status.current }}<br />
+    taken_courses:<br />
+    core
+    <div v-for="(course, i) in this.students.taken_courses" :key="i">
+      <div v-if="course.class.course.category == 'core_courses'">
+        {{ course.class.course.name }} {{ course.grade }} {{ course.class.year }}T{{
+          course.class.trimester
+        }}
+      </div>
+    </div>
+    required
+    <div v-for="(course, i) in this.students.taken_courses" :key="i">
+      <div v-if="course.class.course.category == 'required_courses'">
+        {{ course.class.course.name }} {{ course.grade }} {{ course.class.year }}T{{
+          course.class.trimester
+        }}
+      </div>
+    </div>
+    elective
+    <div v-for="(course, i) in this.students.taken_courses" :key="i">
+      <div v-if="course.class.course.category == 'elective_courses'">
+        {{ course.class.course.name }} {{ course.grade }} {{ course.class.year }}T{{
+          course.class.trimester
+        }}
+      </div>
+    </div>
+    <br /> -->
+    <v-row>
+      <v-col cols="4">
+        <v-card outlined>pic</v-card>
+      </v-col>
+      <v-col>
+        <v-card outlined class="pa-5">
+          <h6>{{ this.students.given_name }} {{ this.students.family_name }}</h6>
+
+          {{ this.students.entry_year }}T{{ this.students.entry_trimester }}<br />
+          Email: {{ this.students.email == null ? " - " : this.students.email }}<br />
+          Line id: {{ this.students.lineID == null ? " - " : this.students.lineID }}<br />
+          Status: {{ this.students.status.current }}<br />
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
@@ -15,6 +60,10 @@ export default {
   },
   data() {
     return {
+      entry: {
+        year: "",
+        tri: "",
+      },
       students: [],
     };
   },
@@ -44,23 +93,20 @@ export default {
                             taken_courses{ 
                                 _id
                                 class{
-                                    course{
-                                    name
-                                    credit
-                                    category
+                                  course{
+                                      name
+                                      credit
+                                      category
+                                    }
+                                  instructor{
+                                      name
+                                    }
+                                  trimester
+                                  year
+                                  section
                                 }
-                                instructor{
-                                    name
-                                }
-                                trimester
-                                year
-                                section
-                            }
-                            student{
-                                given_name
-                            }
-                            grade
-                            isGrading
+                              grade
+                              isGrading
                             }
                         }
                     }
@@ -69,7 +115,8 @@ export default {
         .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
         .then((res) => {
           this.students = res.data.data.student;
-
+          this.entry.year = this.students.entry_year;
+          this.entry.tri = this.students.entry_trimester;
           console.log(this.students);
         })
         .catch((err) => {
@@ -79,3 +126,8 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.cardbg {
+  background: #f8f9fa;
+}
+</style>
