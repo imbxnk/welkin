080d3086e39c5c6064b62ebe96b11ea43f58236f
nhanniee.s <nhanniee.s@gmail.com>
2021-05-12 18:30:24 +0700

    Update search_student.vue

--- main-v2/src/pages/dashboard/students/search_student.vue
+++ main-v2/src/pages/dashboard/students/search_student.vue
@@ -1,14 +1,114 @@
 <template>
   <div id="app">
     Search Student
+    <div class="input-group mb-3">
+      <input
+        type="number"
+        class="form-control"
+        v-model="student_ID"
+        placeholder="You can only search using Student ID. Example: 6080001"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button" @click="getID()">Search</button>
+      </div>
+    </div>
+    <!-- <v-card class="pa-5" style="height:485px;"> </v-card> -->
+    <v-card class="pa-5" style="height:485px;">
+      <div v-if="!check">
+        <h6>Something went wrong..</h6>
+        <p class="caption">Please try again</p>
+      </div>
+      <div v-else>
+        <div v-if="loading" class="loading">
+          <v-progress-circular
+            :size="50"
+            class="ml-16"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
+          <p class="mt-3 primary--text">Searching for {{ search_ID }} ...</p>
+        </div>
+        <div v-else>
+          <div v-if="std_details == null">Sorry, there is no {{ search_ID }} in our database.</div>
+          <div v-else>{{ std_details.given_name }} {{ std_details.family_name }}</div>
+        </div>
+      </div>
+    </v-card>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-
-    }
+      loading: false,
+      check: true,
+      student_ID: "",
+      search_ID: "",
+      std_details: [],
+    };
   },
-}
+  methods: {
+    getID() {
+      if (this.student_ID.length == 7) {
+        this.search_ID = this.student_ID;
+        this.loading = true;
+        this.check = true;
+        this.getStudent(this.search_ID);
+      } else {
+        this.check = false;
+      }
+    },
+    async getStudent(id) {
+      let query = ` query {
+                students (searchInput:{sid:"${id}"}) {
+                  students {
+                    sid
+                    given_name
+                    family_name
+                    email
+                    phone
+                    lineID
+                    status {
+                      current
+                    }
+                    remarks{
+                      message
+                      user {
+                        username
+                      }
+                      _id
+                    }
+                    records{
+                      egci_cumulative_gpa
+                      total_credits
+                      core_credits
+                      major_credits
+                      elective_credits
+
+                    }
+                  }
+                }
+              }
+      `;
+      await this.axios
+        .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
+        .then((res) => {
+          this.std_details = res.data.data.students.students[0];
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
+<style lang="scss" scoped>
+.loading {
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
