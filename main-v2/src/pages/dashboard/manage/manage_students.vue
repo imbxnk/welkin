bcb5d5053d18b9d27d7bf696b5b78b6ee0ce7532
BankNeko <bankneko@gmail.com>
2021-05-24 23:33:54 +0700

    Update manage_students.vue

--- main-v2/src/pages/dashboard/manage/manage_students.vue
+++ main-v2/src/pages/dashboard/manage/manage_students.vue
@@ -1,18 +1,29 @@
 <template>
-  <v-container>
+  <v-container class="mx-auto">
     <div class="d-flex flex-column p-2 bd-highlight">
       <div class="ml-auto p-2 bd-highlight">
         <v-btn @click.prevent="ToImportPage()">Import Student</v-btn>
       </div>
       <div class="p-2 bd-highlight">
         <v-card style="max-width: auto">
-          <v-data-table :headers="headers" :items="students" class="student">
-            <template v-slot:[`item.actions`]="{ item }">
-              <v-icon small @click="editItem(item)">
-                mdi-pencil
-              </v-icon>
-            </template>
-          </v-data-table>
+            <v-data-table
+                :headers="headers"
+                :items="students"
+                class="student"
+            >
+              <template v-slot:item.avatar_url="{ item }">
+                <v-avatar
+                  size="35"
+                  :style="`background: url(${item.avatar_url || 'http://cdn.welkin.app/static/img/no-avatar.jpg'}) center center / cover;`"
+                >
+                </v-avatar>
+              </template>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-icon small @click="editItem(item)">
+                  mdi-pencil
+                </v-icon>
+              </template>
+            </v-data-table>
         </v-card>
       </div>
     </div>
@@ -29,41 +40,42 @@
 
 <script>
 export default {
-  data() {
-    return {
-      headers: [
-        { text: "Student ID", sortable: false, value: "sid", width: 80 },
-        { text: "Prefix", sortable: false, value: "prefix", width: 80 },
-        { text: "Name", sortable: false, value: "name", width: 80 },
-        { text: "Nickname", sortable: false, value: "nick_name", width: 80 },
-        { text: "Email", sortable: false, value: "email", width: 80 },
-        { text: "Phone", sortable: false, value: "phone", width: 80 },
-        { text: "advisor", sortable: false, value: "advisor.name", width: 120 },
-        { text: "Edit", sortable: false, value: "actions", width: 30 },
-      ],
-      students: [],
-      editedIndex: -1,
-      editedItem: {
-        sid: "",
-        prefix: "",
-        given_name: "",
-        family_name: "",
-        nick_name: "",
-        email: "",
-        phone: "",
-        lineID: "",
-        entry_year: "",
-        advisor: {
-          name: "",
-        },
-      },
-      Info: [],
-      dialog: false,
-    };
-  },
-  mounted() {
-    this.getStudents();
-  },
+    data(){
+        return{
+            headers:[
+                { sortable: false, value: "avatar_url", width: "1%" },
+                { text: "Student ID", sortable: false, value: "sid", width: "9%" },
+                { text: "Prefix", sortable: false, value: "prefix", width: "1%" },
+                { text: "Name", sortable: false, value: "name", width: 80 },
+                { text: "Nickname", sortable: false, value: "nick_name", width: 80 },
+                { text: "Email", sortable: false, value: "email", width: 80 },
+                { text: "Phone", sortable: false, value: "phone", width: 80 },
+                { text: "advisor", sortable: false, value: "advisor.name", width: 120 },
+                { text: "Edit", sortable: false, value:"actions", width: 30 },
+            ],
+            students:[],
+            editedIndex: -1,
+            editedItem: {
+                sid: "",
+                prefix: "",
+                given_name: "",
+                family_name: "",
+                nick_name: "",
+                email: "",
+                phone: "",
+                lineID: "",
+                entry_year: "",
+                advisor:{
+                    name: ""
+                },
+            },
+            Info: [],
+            dialog: false,
+        }
+    },
+    mounted(){
+        this.getStudents();
+    },
   methods: {
     ToImportPage() {
       this.$router.push({ name: "add_student" });
@@ -80,6 +92,7 @@ export default {
                     email
                     phone
                     lineID
+                    avatar_url
                     entry_year
                     prefix
                     advisor {
