94390982edd55e3fb464eab27df0b791bd1b2df9
BankNeko <bankneko@gmail.com>
2021-05-26 18:17:13 +0700

    Update manage_website.vue

--- main-v2/src/pages/dashboard/manage/manage_website.vue
+++ main-v2/src/pages/dashboard/manage/manage_website.vue
@@ -1,96 +1,68 @@
 <template>
+<div id="app">
   <v-card
+    id="batches"
     class="mx-auto"
     max-width="500"
   >
-    <v-toolbar
-      flat
-      color="transparent"
-    >
-      <v-toolbar-title>Batches</v-toolbar-title>
-      <v-spacer></v-spacer>
+    <div class="d-flex align-items-center mx-4 pt-4">
+      <div class="d-flex flex-column flex-grow-1">
+        <h5 class="m-0">Batches</h5>
+        <span style="font-size: 0.9rem; opacity: 0.8">Select the batches to show on the website</span>
+      </div>
       <v-btn
         icon
         @click="updateConfig()"
       >
-        <v-icon>mdi-check</v-icon>
+        <v-icon v-if="!loading">mdi-check</v-icon>
+        <div v-else class="wk-spinner mx-auto my-4"></div>
       </v-btn>
-    </v-toolbar>
+    </div>
 
     <v-container>
-      <div style="color: #999;" v-if="selected.length === 0">No batches</div>
-      <v-row
-        class="m-0"
-        align="center"
-        justify="start"
+      <div
+        class="m-0 d-flex flex-wrap flex-column"
+        style="background: #efefef6e; border-radius: 20px; padding: 5px"
       >
-        <v-col
-          v-for="(selection, i) in selections"
-          :key="selection"
-          class="shrink wk-chip"
-          style="min-width: unset !important;"
-        >
-          <v-chip
-            :disabled="loading"
-            close
-            @click:close="selected.splice(i, 1)"
+        <div style="color: #999; font-size: 0.9rem; padding: 0 5px" v-if="selected.length === 0">No batches selected</div>
+        <div style="color: #666; font-size: 0.8rem; padding: 0 5px" v-else>Selected Batches</div>
+        <div class="d-flex flex-wrap">
+          <template
+            v-for="(selection, i) in selections"
           >
-            {{ selection }}
-          </v-chip>
-        </v-col>
-      </v-row>
+            <div :key="selection" class="wk-chip">
+              <v-chip
+                :disabled="loading"
+                close
+                @click:close="selected.splice(i, 1)"
+              >
+                <span class="wk-chip-text">{{ selection }}</span>
+              </v-chip>
+            </div>
+          </template>
+        </div>
+      </div>
     </v-container>
     <v-divider v-if="!allSelected"></v-divider>
     <v-container class="pt-0" v-if="!allSelected">
-      <!-- <v-col
-          cols="12"
-        >
-          <v-text-field
-            ref="search"
-            v-model="search"
-            full-width
-            hide-details
-            label="Search"
-            single-line
-          ></v-text-field>
-      </v-col> -->
-      <!-- <v-list v-if="!allSelected">
-        <template v-for="item in categories">
-          <v-list-item
-            v-if="!selected.includes(item)"
-            :key="item"
-            :disabled="loading"
-            @click="selected.push(item)"
-          >
-            <v-list-item-title v-text="item"></v-list-item-title>
-          </v-list-item>
-        </template>
-      </v-list> -->
-
+      <div style="font-size: 0.8rem; color: #666; padding: 0 5px" v-if="!allSelected">Available Batches</div>
       <div class="d-flex align-items-center">
         <template v-for="item in categories">
-          <div class="wk-chip" :key="item">
+          <div v-if="!selected.includes(item)" class="wk-chip" :key="item">
             <v-chip
-              v-if="!selected.includes(item)"
               :disabled="loading"
               @click="selected.push(item)"
             >
-              {{ item }}
+              <span class="wk-chip-text">{{ item }}</span>
             </v-chip>
           </div>
         </template>
       </div>
     </v-container>
-
-    <!-- <v-divider v-if="!allSelected"></v-divider> -->
-
-
-
-    <!-- <v-divider></v-divider> -->
-
     <v-card-actions>
     </v-card-actions>
   </v-card>
+</div>
 </template>
 
 <script>
@@ -168,6 +140,7 @@ export default {
         });
     },
     updateConfig() {
+      this.loading = true
       let query = `
           mutation {
             updateConfig(configInput: {
@@ -178,10 +151,14 @@ export default {
       this.axios
         .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
         .then((res) => {
+          this.loading = false
+          if (res.data.data.updateConfig.success) {
+            this.$config.selectedBatches = this.selected
+          }
           console.log(res.data)
         })
         .catch((err) => {
-          // this.loading = false;
+          this.loading = false
         });
     },
   },
@@ -190,6 +167,34 @@ export default {
 
 <style scoped>
 .wk-chip {
-  padding: 10px 10px 0 0;
+  padding: 4px;
+}
+
+.wk-chip .v-chip .v-chip__content .wk-chip-text {
+  max-width: 124px !important;
+  white-space: nowrap !important;
+  overflow: hidden !important;
+  text-overflow: ellipsis !important;
+}
+
+.wk-spinner {
+	pointer-events: none;
+	width: 1.5em;
+	height: 1.5em;
+	border: 0.3em solid transparent;
+	border-color: #eee;
+	border-top-color: #3E67EC;
+	border-radius: 50%;
+	animation: loadingspin 1s linear infinite;
+}
+
+h5 {
+  font-weight: 700;
+}
+
+@keyframes loadingspin {
+	100% {
+		transform: rotate(360deg)
+	}
 }
 </style>
