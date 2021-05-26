1ee12008135f46bc46779835eb86bbd5b92fc5ae
phatac131 <phattharaporn.rod@student.mahidol.edu>
2021-05-26 15:26:20 +0700

    update manage_student

--- main-v2/src/pages/dashboard/manage/manage_students.vue
+++ main-v2/src/pages/dashboard/manage/manage_students.vue
@@ -17,11 +17,56 @@
       </div>
     </div>
 
-    <v-dialog v-model="dialog">
+    <v-dialog v-model="dialog" max-width="1000px">
       <v-card>
         <v-card-title>
-          <h5>Test</h5>
+            Edit Student
         </v-card-title>
+        <v-card-text>
+            <v-form>
+                <div class="d-flex flex-column bd-highlight">
+                    <div class="p-2 bd-highlight">
+                        <div class="d-flex flex-row justify-content-evenly bd-highlight">
+                            <div class="p-2 bd-highlight">
+                                <v-select class="input" label="Prefix" :items="prefix" outlined v-model="editedItem.prefix"></v-select>
+                            </div>
+                            <div class="p-2 bd-highlight"> 
+                                <v-text-field class="input" label="First name" v-model="editedItem.given_name" outlined></v-text-field>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <v-text-field class="input" label="Family name" v-model="editedItem.family_name" outlined></v-text-field>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <div class="d-flex flex-row justify-content-evenly bd-highlight">
+                            <div class="p-2 bd-highlight">
+                                <v-text-field class="input" label="Entry Trimester" v-model="editedItem.entry_trimester" outlined></v-text-field>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <v-text-field class="input" label="Entry Year" v-model="editedItem.entry_year" outlined></v-text-field>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <v-text-field class="input" label="Email" v-model="editedItem.email" outlined></v-text-field>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <div class="d-flex flex-row justify-content-evenly bd-highlight">
+                            <div class="p-2 bd-highlight">
+                                <v-text-field class="input" label="Phone number" v-model="editedItem.phone" outlined></v-text-field>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <div class="d-flex flex-row justify-content-end bd-highlight">
+                            <v-btn class="my-3" @click="close()" text>close</v-btn>
+                            <v-btn class="my-3" color="#3c84fb" @click="save()" text>Submit</v-btn>
+                        </div>
+                    </div>
+                </div>
+            </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </v-container>
@@ -38,6 +83,8 @@ export default {
         { text: "Nickname", sortable: false, value: "nick_name", width: 80 },
         { text: "Email", sortable: false, value: "email", width: 80 },
         { text: "Phone", sortable: false, value: "phone", width: 80 },
+        { text: "Entry Trimester", sortable: false, value: "entry_trimester", width: 80 },
+        { text: "Entry Year", sortable: false, value: "entry_year", width: 80 },
         { text: "advisor", sortable: false, value: "advisor.name", width: 120 },
         { text: "Edit", sortable: false, value: "actions", width: 30 },
       ],
@@ -52,13 +99,24 @@ export default {
         email: "",
         phone: "",
         lineID: "",
+        entry_trimester: "",
         entry_year: "",
-        advisor: {
-          name: "",
-        },
+      },
+      defaultItem: {
+        sid: "",
+        prefix: "",
+        given_name: "",
+        family_name: "",
+        nick_name: "",
+        email: "",
+        phone: "",
+        lineID: "",
+        entry_trimester: "",
+        entry_year: "",
       },
       Info: [],
       dialog: false,
+      prefix:["Mr.", "Ms.", "Mrs",],
     };
   },
   mounted() {
@@ -80,6 +138,7 @@ export default {
                     email
                     phone
                     lineID
+                    entry_trimester
                     entry_year
                     prefix
                     advisor {
@@ -105,16 +164,65 @@ export default {
           console.log(err);
         });
     },
-
+    
     editItem(item) {
-      this.editedIndex = this.Info.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      // eslint-disable-next-line no-console
-      console.log(item);
-      this.dialog = true;
+        this.editedIndex = this.Info.indexOf(item);
+        this.editedItem = Object.assign({}, item);
+        // eslint-disable-next-line no-console
+        console.log(item,);
+        this.dialog = true;
+    },
+    
+    close() {
+        this.dialog = false;
+        this.$nextTick(() => {
+            this.editedItem = Object.assign({}, this.defaultItem);
+            this.editedIndex = -1;
+        });
+    },
+    save() {
+        if (this.editedIndex > -1) {
+            Object.assign(this.Info[this.editedIndex], this.editedItem);
+        } else {
+            this.Info.push(this.editedItem);
+        }
+        // eslint-disable-next-line no-console
+        console.log(this.Info[this.editedIndex], this.Info);
+        let query = `mutation{
+                        updateStudent(searchInput: {sid:"${this.Info[this.editedIndex].sid}"},studentInput: {
+                            sid: "${this.Info[this.editedIndex].sid}"
+                            given_name: "${this.Info[this.editedIndex].given_name}",
+                            family_name: "${this.Info[this.editedIndex].family_name}",
+                            nick_name: "${this.Info[this.editedIndex].nick_name}"
+                            prefix: "${this.Info[this.editedIndex].prefix}",
+                            entry_trimester: "${this.Info[this.editedIndex].entry_trimester}",
+                            entry_year: "${this.Info[this.editedIndex].entry_year}",
+                            email: "${this.Info[this.editedIndex].email}",
+                            phone: "${this.Info[this.editedIndex].phone}",
+                            ){
+                                sid
+                                given_name
+                                family_name
+                                nick_name
+                                prefix
+                                email
+                                phone
+                            }
+                        }`
+        this.axios.post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials : true }).then(res => {
+            this.close()
+        }).catch (err =>{
+            console.log(err)
+        })
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+    .input{
+        min-width: 50px;
+        max-width: 300px;
+        width: 300px;
+    }
+</style>
