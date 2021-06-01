208f824cb63d2ad7c43e89cd5cba133068526381
Miqly <42849871+knmixsiri@users.noreply.github.com>
2021-06-02 00:39:33 +0700

    Update add_students.vue

--- main-v3/src/pages/dashboard/manage/add_students.vue
+++ main-v3/src/pages/dashboard/manage/add_students.vue
@@ -25,11 +25,11 @@
           <div class="d-flex flex-row bd-highlight justify-content-center align-items-center">
             <div class="p-2 flex-sm-grow-1 bd-highlight md-12">
               <v-card v-if="importFile" color="#97b8f0" class="mb-12" style="min-width: 100px">
-            <v-card-text>
-              <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"  @vdropzone-success="selectFile" style="min-width:80px" lazy-validation>
-              </vue-dropzone>
-            </v-card-text>
-          </v-card>
+                <v-card-text>
+                  <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"  @vdropzone-success="selectFile" style="min-width:80px" lazy-validation>
+                  </vue-dropzone>
+                </v-card-text>
+              </v-card>
               <v-form v-if="addManually" ref="form" lazy-validation style="min-width:300px">
                 <div class="d-flex flex-md-row flex-column bd-highlight justify-content-center align-items-center">
                   <div class="p-2 bd-highlight">
@@ -76,19 +76,16 @@
               </v-btn>
             </div>
             <div class="ms-auto p-2 bd-highlight justify-content-sm-start">
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn class="speed-dial" dark color="#3c84fb" @click.prevent="toggleForm()" v-bind="attrs" v-on="on">
-                  <v-icon>
-                    mdi-account-circle
-                  </v-icon>
-                </v-btn>
-                  </template>
-                  <span>Change to add manually or import files</span>
-                </v-tooltip>
-
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn class="speed-dial" dark color="#3c84fb" @click.prevent="toggleForm()" v-bind="attrs" v-on="on">
+                    <v-icon>mdi-account-circle</v-icon>
+                  </v-btn>
+                </template>
+                <span>Change to add manually or import files</span>
+              </v-tooltip>
             </div>
-        </div>
+          </div>
         </v-stepper-content>
 
         <v-stepper-step :complete="e6 > 2" class="my-auto py-auto" step="2">
@@ -101,54 +98,52 @@
               <select class="form-control" name="sheetName" id="sheetName" label="Seleted a term" v-show="fileStatus" @change="getSelectedValue($event)" style="max-width:300px; min-width:50px">
                 <option selected>Please select a term</option>
                 <option v-for="(item, index) in this.sheetNames" :key="index" :value="item">{{ item }}</option>
-            </select>
+              </select>
             </div>
             <div class="p-2 bd-highlight">
-                <v-simple-table height="300px" class="mx-auto my-auto py-2 px-2">
-                      <template v-slot:default>
-                      <thead>
-                          <tr>
-                          <th class="text-left">
-                              ID
-                          </th>
-                          <th class="text-left">
-                              Program
-                          </th>
-                          <th class="text-left">
-                              Prefix
-                          </th>
-                          <th class="text-left">
-                              First Name
-                          </th>
-                          <th class="text-left">
-                              Family Name
-                          </th>
-                          <th class="text-left">
-                              Advisor
-                          </th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr
-                          v-for="(item, index) in studentsData"
-                          :key="index"
-                          :value="item"
-                          >
-                          <td>{{ item.ID }}</td>
-                          <td>{{ item.Program }}</td>
-                          <td>{{ item.Prefix }}</td>
-                          <td>{{ item.Name }}</td>
-                          <td>{{ item.LastName }}</td>
-                          <td>{{ item.Advisor }}</td>
-                          </tr>
-                      </tbody>
-                      </template>
-                </v-simple-table>
+              <v-simple-table height="300px" class="mx-auto my-auto py-2 px-2">
+                <template v-slot:default>
+                <thead>
+                  <tr>
+                  <th class="text-left">
+                      ID
+                  </th>
+                  <th class="text-left">
+                      Program
+                  </th>
+                  <th class="text-left">
+                      Prefix
+                  </th>
+                  <th class="text-left">
+                      First Name
+                  </th>
+                  <th class="text-left">
+                      Family Name
+                  </th>
+                  <th class="text-left">
+                      Advisor
+                  </th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    <tr
+                    v-for="(item, index) in studentsData"
+                    :key="index"
+                    :value="item"
+                    >
+                    <td>{{ item.ID }}</td>
+                    <td>{{ item.Program }}</td>
+                    <td>{{ item.Prefix }}</td>
+                    <td>{{ item.Name }}</td>
+                    <td>{{ item.LastName }}</td>
+                    <td>{{ item.Advisor }}</td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
             </div>
             <div class="p-2 bd-highlight">
-              <v-btn  color="#3c84fb" class="my-auto" @click="e6 = 3; upload()" style="color: white;">
-                Upload
-              </v-btn>
+              <v-btn  color="#3c84fb" class="my-auto" @click="e6 = 3; upload()" style="color: white;">Upload</v-btn>
               <v-btn color="#3c84fb" text @click="cancelStep2()">Cancel</v-btn>
             </div>
           </div>
@@ -159,80 +154,77 @@
         </v-stepper-step>
         <v-stepper-content step="3" class="my-3">
           <v-simple-table height="300px" class="mx-auto my-auto py-2 px-2">
-                      <template v-slot:default>
-                      <thead>
-                          <tr>
-                          <th class="text-left">
-                              ID
-                          </th>
-                          <th class="text-left">
-                              Program
-                          </th>
-                          <th class="text-left">
-                              Prefix
-                          </th>
-                          <th class="text-left">
-                              First Name
-                          </th>
-                          <th class="text-left">
-                              Family Name
-                          </th>
-                          <th class="text-left">
-                              Advisor
-                          </th>
-                          <th class="text-left">
-                              Status
-                          </th>
-                          </tr>
-                      </thead>
-                      <tbody v-if="!duplicateStatus">
-                          <tr
-                          class="success"
-                          v-for="(item, index) in studentsData"
-                          :key="index"
-                          :value="item"
-                          >
-                          <td>{{ item.ID }}</td>
-                          <td>{{ item.Program }}</td>
-                          <td>{{ item.Prefix }}</td>
-                          <td>{{ item.Name }}</td>
-                          <td>{{ item.LastName }}</td>
-                          <td>{{ item.Advisor }}</td>
-                          </tr>
-                      </tbody>
-                      <tbody v-else-if="duplicateStatus">
-                          <tr
-                          class="duplicate"
-                          v-for="(item, index) in duplicateStudents"
-                          :key="index"
-                          :value="item"
-                          >
-                          <td>{{ item.ID }}</td>
-                          <td>{{ item.Program }}</td>
-                          <td>{{ item.Prefix }}</td>
-                          <td>{{ item.Name }}</td>
-                          <td>{{ item.LastName }}</td>
-                          <td>{{ item.Advisor }}</td>
-                          </tr>
-                      </tbody>
-                      </template>
-                </v-simple-table>
-                <div class="d-flex justify-content-end">
-                  <div class="p-2 bd-highlight">
-                    <v-btn color="#3c84fb" text @click="cancelStep3()">
-                      Cancel
-                    </v-btn>
-                  </div>
-                </div>
+            <template v-slot:default>
+            <thead>
+              <tr>
+              <th class="text-left">
+                  ID
+              </th>
+              <th class="text-left">
+                  Program
+              </th>
+              <th class="text-left">
+                  Prefix
+              </th>
+              <th class="text-left">
+                  First Name
+              </th>
+              <th class="text-left">
+                  Family Name
+              </th>
+              <th class="text-left">
+                  Advisor
+              </th>
+              <th class="text-left">
+                  Status
+              </th>
+              </tr>
+            </thead>
+            <tbody v-if="!duplicateStatus">
+              <tr
+              class="success"
+              v-for="(item, index) in studentsData"
+              :key="index"
+              :value="item"
+              >
+              <td>{{ item.ID }}</td>
+              <td>{{ item.Program }}</td>
+              <td>{{ item.Prefix }}</td>
+              <td>{{ item.Name }}</td>
+              <td>{{ item.LastName }}</td>
+              <td>{{ item.Advisor }}</td>
+              </tr>
+            </tbody>
+            <tbody v-else-if="duplicateStatus">
+              <tr
+              class="duplicate"
+              v-for="(item, index) in duplicateStudents"
+              :key="index"
+              :value="item"
+              >
+              <td>{{ item.ID }}</td>
+              <td>{{ item.Program }}</td>
+              <td>{{ item.Prefix }}</td>
+              <td>{{ item.Name }}</td>
+              <td>{{ item.LastName }}</td>
+              <td>{{ item.Advisor }}</td>
+              </tr>
+            </tbody>
+            </template>
+          </v-simple-table>
+          <div class="d-flex justify-content-end">
+            <div class="p-2 bd-highlight">
+              <v-btn color="#3c84fb" text @click="cancelStep3()">
+                Cancel
+              </v-btn>
+            </div>
+          </div>
         </v-stepper-content>
-
       </v-stepper>
     </div>
-      
   </div>
   </v-container>
 </template>
-
 
 <script>
 import XLSX from 'xlsx'
@@ -256,8 +248,8 @@ import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
         //manual
         prefix:["Mr.", "Ms.", "Mrs",],
-        icProgram:[ 'ICBE',
-                    'ICCI',
+        icProgram:[ 'ICCI',
+                    'ICBE',
                     'ICCS',
                     'ICMI',
                     'ICMF',
@@ -304,7 +296,7 @@ import 'vue2-dropzone/dist/vue2Dropzone.min.css'
       }
     },
     components:{
-        vueDropzone: vue2Dropzone
+      vueDropzone: vue2Dropzone
     },
     methods:{
       toggleForm(){
@@ -314,32 +306,32 @@ import 'vue2-dropzone/dist/vue2Dropzone.min.css'
       },
       selectFile(file){
         //show file is already upload
-          this.fileStep = true
+        this.fileStep = true
         //show the select component
-          this.fileStatus = true
+        this.fileStatus = true
         //get the selected file' info
         this.selectedFile = file;
         XLSX.utils.json_to_sheet(this.data,'out.xlsx');
         //if file is selected
         if(this.selectedFile){
-            this.uploadSuccess = true
-            //read data and store it in our variable
-            let fileReader = new FileReader();
-            fileReader.readAsBinaryString(this.selectedFile);
-            fileReader.onload = (event) => {
-            //the data is unreadable
-            let data = event.target.result;
-            //we have to convert it and store in our variable
-            this.workbook = XLSX.read(data, {type: "binary"});
-            //save the data, so we can use later in other functions
-            this.sheetNames = this.workbook.SheetNames;
-            this.sheetName = this.sheetNames[0];
-            this.studentsData = this.readMyFile(this.workbook,this.sheetName);
-            // get entry_year and entry_trimester
-            [this.entryYear,this.entryTrimester] = this.sheetName.split("T");
-            }
+          this.uploadSuccess = true
+          //read data and store it in our variable
+          let fileReader = new FileReader();
+          fileReader.readAsBinaryString(this.selectedFile);
+          fileReader.onload = (event) => {
+          //the data is unreadable
+          let data = event.target.result;
+          //we have to convert it and store in our variable
+          this.workbook = XLSX.read(data, {type: "binary"});
+          //save the data, so we can use later in other functions
+          this.sheetNames = this.workbook.SheetNames;
+          this.sheetName = this.sheetNames[0];
+          this.studentsData = this.readMyFile(this.workbook,this.sheetName);
+          // get entry_year and entry_trimester
+          [this.entryYear,this.entryTrimester] = this.sheetName.split("T");
           }
-        },
+        }
+      },
       submitForm(){
         if(this.manuallyData == {}){
           console.log(this.studentsData)
@@ -366,63 +358,63 @@ import 'vue2-dropzone/dist/vue2Dropzone.min.css'
         [this.entryYear,this.entryTrimester] = this.sheetName.split("T");
       },
       readMyFile: function (workbook,currentSheetName){
-          return XLSX.utils.sheet_to_row_object_array(workbook.Sheets[currentSheetName]);
+        return XLSX.utils.sheet_to_row_object_array(workbook.Sheets[currentSheetName]);
       },
       upload:async function () {
-            //clear the duplicatedStudents
-            this.duplicateStudents = [];
-            //get entry_year and entry_trimester
-            [this.entryYear,this.entryTrimester] = this.sheetName.split("T");
-            let students = {...this.studentsData}
-            for(var i in students) {
-                let std = {...students[i]};
-                //post graphql by using axios
-                std.Advisor = std.Advisor.trim().split(". ").slice(-1).pop().trim()
-                let gql = `
-                        mutation{
-                            addStudent ( studentInput: {
-                                    sid: "${std.ID}",
-                                    prefix: "${std.Prefix}",
-                                    given_name: "${std.Name}",
-                                    family_name: "${std.LastName}",
-                                    program: "${std.Program}",
-                                    entry_trimester: "${this.entryTrimester}",
-                                    entry_year: "${this.entryYear}",
-                                    advisor_name: "${std.Advisor}"
-                                }
-                            ){
-                                sid
-                                given_name
-                            }
-                        }
-                    `
-                await this.axios.post(this.url, { query : gql }, { withCredentials: true }).then(res => {
-                    console.log(res);
-                    this.addingSuccessStatus = true
-                }).catch (err => {
-                    console.log(err);
-                    this.duplicateStudents.push(std);
-                    this.duplicateStatus = true
-                })
+        //clear the duplicatedStudents
+        this.duplicateStudents = [];
+        //get entry_year and entry_trimester
+        [this.entryYear,this.entryTrimester] = this.sheetName.split("T");
+        let students = {...this.studentsData}
+        for(var i in students) {
+          let std = {...students[i]};
+          //post graphql by using axios
+          std.Advisor = std.Advisor.trim().split(". ").slice(-1).pop().trim()
+          let gql = `
+            mutation{
+              addStudent ( studentInput: {
+                sid: "${std.ID}",
+                prefix: "${std.Prefix}",
+                given_name: "${std.Name}",
+                family_name: "${std.LastName}",
+                program: "${std.Program}",
+                entry_trimester: "${this.entryTrimester}",
+                entry_year: "${this.entryYear}",
+                advisor_name: "${std.Advisor}"
+              }
+              ){
+                sid
+                given_name
+              }
             }
-        },
-        cancelStep1(){
-          this.manuallyData = {}
-          this.selectedFile = {}
-        },
-        cancelStep2(){
-          this.e6 = 1
-          this.manuallyData = {}
-          this.selectedFile = {}
-        },
-        cancelStep3(){
-          this.e6 = 2
-        },
-        ToEditPage() {
-          this.$router.push({ name: "manage_student" });
-        },
-        toStep2(){
+          `
+          await this.axios.post(this.url, { query : gql }, { withCredentials: true }).then(res => {
+            console.log(res);
+            this.addingSuccessStatus = true
+          }).catch (err => {
+            console.log(err);
+            this.duplicateStudents.push(std);
+            this.duplicateStatus = true
+          })
         }
+      },
+      cancelStep1(){
+        this.manuallyData = {}
+        this.selectedFile = {}
+      },
+      cancelStep2(){
+        this.e6 = 2
+        this.manuallyData = {}
+        this.selectedFile = {}
+      },
+      cancelStep3(){
+        this.e6 = 3
+      },
+      ToEditPage() {
+        this.$router.push({ name: "manage_student" });
+      },
+      toStep2(){
+      }
     }
   }
 </script>
