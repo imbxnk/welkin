f529f8a59cbac0e4d3036bcb5a49bdaecb8a6de9
BankNeko <bankneko@gmail.com>
2021-06-04 16:18:28 +0700

    Update show_announce.vue

--- main-v2/src/pages/dashboard/home/components/show_announce.vue
+++ main-v2/src/pages/dashboard/home/components/show_announce.vue
@@ -1,10 +1,14 @@
 <template>
-  <div style="height:150px">
+  <div style="max-height:150px">
     <h6 class="primary--text pl-2">Announcements</h6>
     <simplebar class="ex1">
-      <v-card v-for="(announcement, i) in $config.announcements" :key="i" class="announce-card "
-        >{{ announcement.content }} - {{ announcement.user.display_name || announcement.user.username }}</v-card
-      >
+      <v-card v-for="(announcement, i) in $config.announcements" :key="i" class="announce-card ">
+        <v-card-title class="m-0 p-0 d-flex justify-content-between" style="font-size: 1.1rem">
+          {{ announcement.title }}
+          <v-card-subtitle class="m-0 p-0">{{ readableDate(announcement.createdAt) + " - " + readableDate(announcement.endDate)}}</v-card-subtitle>
+        </v-card-title>
+        <v-card-text class="m-0 p-0 pt-2" v-html="announcement.content"></v-card-text>
+      </v-card>
     </simplebar>
   </div>
 </template>
@@ -20,11 +24,16 @@ export default {
 
     };
   },
+  methods: {
+    readableDate(date) {
+      return this.moment(parseInt(date)).format("DD MMMM YYYY (HH:MM)");
+    },
+  },
 };
 </script>
 <style>
 .ex1 {
-  height: 130px;
+  max-height: 130px;
   /* overflow: auto; */
 }
 .announce-card {
