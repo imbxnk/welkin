7a3e4c279c3b393949217e4cfe111201851494d8
BankNeko <bankneko@gmail.com>
2021-06-16 20:50:14 +0700

    remove unused files

--- api-v2/utils/authorizedGroups.js
+++
@@ -1,8 +0,0 @@
-const ErrorHandler = require('./errorHandlers')
-
-exports.authorizedGroups = (groups, user) => {
-  let authGroups = [...groups]
-  if(!authGroups.includes(user.group)) {
-      throw new ErrorHandler('Unauthorized.', 401)
-  }
-}
