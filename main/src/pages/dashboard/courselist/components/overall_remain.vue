8a13309c3f91c997bcd49aba6a65541313af281e
nhanniee.s <nhanniee.s@gmail.com>
2021-06-23 16:52:28 +0700

    Update overall_remain.vue

--- main/src/pages/dashboard/courselist/components/overall_remain.vue
+++ main/src/pages/dashboard/courselist/components/overall_remain.vue
@@ -4,16 +4,17 @@
 
     <table class="table table-hover">
       <tr>
-        <th rowspan="2">Course Type</th>
-        <th rowspan="2">Course Name</th>
+        <th rowspan="2" align="center">Course Type</th>
+        <th rowspan="2" align="center">Course Name</th>
         <th colspan="6" align="center">Remain Student</th>
       </tr>
       <tr>
         <th v-for="(batch, i) in this.$config.selectedBatches" :key="i">{{ batch }}</th>
       </tr>
       <tr v-for="(course, i) in courses" :key="i">
-        <td>course type</td>
+        <td align="center">{{ course.category }}</td>
         <td>{{ course.name }}</td>
+        <td>0</td>
         <td>0</td>
         <td>0</td>
         <td>0</td>
@@ -30,7 +31,7 @@ export default {
   created() {},
   data() {
     return {
-      courses: [],
+      courses: {},
     };
   },
   mounted() {
@@ -46,15 +47,25 @@ export default {
                 courses {
                   name
                   code
+                  category
                 }
               }
             }
           `;
       query = query.replace(/\s+/g, " ").trim();
+
       this.axios
         .post(process.env.VUE_APP_GRAPHQL_URL, { query }, { withCredentials: true })
         .then((res) => {
           this.courses = [...res.data.data.courses.courses];
+          this.courses.forEach((course) => {
+            // course.category = course.category.join(", ");
+            course.category = course.category
+              .join(", ")
+              .replace("core_course", "C")
+              .replace("required_courses", "R")
+              .replace("elective_courses", "E");
+          });
           console.log(this.courses);
         })
         .catch((err) => {
