9b9d331d3820802dcd87e82ea426d47cb4365ca0
nhanniee.s <nhanniee.s@gmail.com>
2021-06-23 17:04:56 +0700

    back

--- main/src/pages/dashboard/courselist/components/overall_remain.vue
+++ main/src/pages/dashboard/courselist/components/overall_remain.vue
@@ -1,27 +1,45 @@
 <template>
-  <v-card class=" pa-3">
-    <v-card-title>Overall Student Registration</v-card-title>
+  <div id="app">
+    <a @click="$router.back()" class="overline my-n1 back primary--text"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-arrow-left"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+        />
+      </svg>
+      BACK</a
+    >
+    <v-card class=" pa-3">
+      <v-card-title>Overall Student Registration</v-card-title>
 
-    <table class="table table-hover">
-      <tr>
-        <th rowspan="2" align="center">Course Type</th>
-        <th rowspan="2" align="center">Course Name</th>
-        <th colspan="6" align="center">Remain Student</th>
-      </tr>
-      <tr>
-        <th v-for="(batch, i) in this.$config.selectedBatches" :key="i">{{ batch }}</th>
-      </tr>
-      <tr v-for="(course, i) in courses" :key="i">
-        <td align="center">{{ course.category }}</td>
-        <td>{{ course.name }}</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-      </tr>
-    </table>
-  </v-card>
+      <table class="table table-hover">
+        <tr>
+          <th rowspan="2" align="center">Course Type</th>
+          <th rowspan="2" align="center">Course Name</th>
+          <th colspan="6" align="center">Remain Student</th>
+        </tr>
+        <tr>
+          <th v-for="(batch, i) in this.$config.selectedBatches" :key="i">{{ batch }}</th>
+        </tr>
+        <tr v-for="(course, i) in courses" :key="i">
+          <td align="center">{{ course.category }}</td>
+          <td>{{ course.name }}</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+      </table>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
